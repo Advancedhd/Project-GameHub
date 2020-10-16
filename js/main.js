aae8e6ff84f7e037e9522c8c48a69ad2e6d79f82
Advancedhd <hakon.volden@gmail.com>
2020-10-16 16:43:43 +0200

    added hamburger

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    
+
+    burger.addEventListener('click', () => {
+        /*Toggle Hamburger*/
+        nav.classList.toggle('nav-active');
+
+        /*toggle animated links*/
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = link.style.animation = `navLinkfade 0.5s ease forwards ${index / 5 + 1.0}s`;
+            }
+        });
+        /*animated lines*/
+        burger.classList.toggle('toggle');
+    });
+}
+
+
+navSlide();
