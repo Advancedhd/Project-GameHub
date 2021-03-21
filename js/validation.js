74d693000960adef50ec9a3983cde961f869e21d
Advancedhd <hakon.volden@gmail.com>
2021-03-21 19:29:55 +0100

    as

--- js/validation.js
+++ js/validation.js
@@ -1,24 +1,3 @@
-/* <div class="feedback">
-    <h1 class="feedbacktext"><b>Feedback</b></h1>
-        <div class="feedbackcontent">
-            <label for="firstname" class="signuplabel" >Your Name</label>
-            <input type="text" name="firstname" id="firstname">
-        </div>
-         <div class="feedbackcontent">
-            <label for="email"  class="signuplabel">Email Address</label>
-            <input type="text" name="email" id="email">
-        </div>
-        <div class="feedbackcontent">
-            <label for="subject">Subject</label>
-            <textarea id="subject" name="subject" placeholder="Your Feedback" style="height:200px"></textarea>
-        </div>
-        <div>
-            <button class="feedbacksend">Send</button>
-        </div>
-</div> */
-
-
-
 
 const form = document.querySelector(".checkform");
 const namefield = document.querySelector(".NameField");
@@ -63,8 +42,8 @@ let validateForm = () => {
         msg += "Please enter a valid email! <br>"
         validationFlag = false
     }
-    if(!checklength(subject.value, 19)) {
-        msg += "Subject must be minimum 20 in length! <br>"
+    if(!checklength(subject.value, 14)) {
+        msg += "Subject must be minimum 15 in length! <br>"
         validationFlag = false
     } 
 
@@ -74,7 +53,7 @@ let validateForm = () => {
         msg = ""
     }, 10000);
     if (validationFlag === true) {
-        validated.innerHTML = "Succesfully Validated"
+        validated.innerHTML = "Feedback Sent"
     }
 }
 
